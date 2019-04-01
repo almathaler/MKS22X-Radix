a47f0e181d8d718b4125b402ed463ba8807a21b7
almathaler <athaler00@stuy.edu>
2019-04-01 11:01:36 -0400

    might need to use arrayList nstead of array

---
+++ Radix.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class Radix{
+  public static void radixsort(int[]data){
+      MyLinkedList[] digits = new MyLinkedList[20]; //20 so that we can do negatives
+
+  }
+  public static Integer getDigit(int place){
+    //
+  }
+}
