651c1ee8d91ef3a49815727d1e13455d3d33f2f2
almathaler <athaler00@stuy.edu>
2019-04-02 11:03:08 -0400

    works normally but trying to speed it up. remember to make longestnum abs val if u revert and if num is negative put at digFreq[9+placer] not -

--- Radix.java
+++ Radix.java
@@ -2,66 +2,116 @@ import java.util.ArrayList;
 import java.lang.IllegalArgumentException;
 import java.util.Arrays;
 public class Radix{
-  public static void main(String[] args){
-    System.out.println("The 3rd digit of 4321: " + getDigit(4321, 3));
-    int[] data = {13, 14, 42, 31, 5, 4, 2, 23, 90, 6, 5, 4, 31};
-    System.out.println("data: " + Arrays.toString(data));
-    radixsort(data);
-    System.out.println("sorted: " + Arrays.toString(data));
+  public static void main(String[]args){
+  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+  int[]MAX_LIST = {10};
+  for(int MAX : MAX_LIST){
+    for(int size = 25; size < 26; size*=2){
+      long qtime=0;
+      long btime=0;
+      //average of 5 sorts.
+      for(int trial = 0 ; trial <=5; trial++){
+        int []data1 = new int[size];
+        int []data2 = new int[size];
+        for(int i = 0; i < data1.length; i++){
+          data1[i] = (int)(Math.random()*MAX);
+          data2[i] = data1[i];
+        }
+        long t1,t2;
+        t1 = System.currentTimeMillis();
+        radixsort(data2);
+        t2 = System.currentTimeMillis();
+        qtime += t2 - t1;
+        t1 = System.currentTimeMillis();
+        Arrays.sort(data1);
+        t2 = System.currentTimeMillis();
+        btime+= t2 - t1;
+        if(!Arrays.equals(data1,data2)){
+          System.out.println("FAIL TO SORT!");
+          System.out.println(Arrays.toString(data1) +"\n" + Arrays.toString(data2));
+          System.exit(0);
+        }
+      }
+      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+    }
+    System.out.println();
   }
+}
   @SuppressWarnings("unchecked")
   public static void radixsort(int[]data){
-      //MyLinkedList<Integer>[] digFreq = new MyLinkedList<Integer>[20]; //20 so that we can do negatives
+      //create the buckets and fill it w LLs
       MyLinkedList<Integer>[] digFreq = new MyLinkedList[20];
       for (int a = 0; a<digFreq.length; a++){
         digFreq[a] = new MyLinkedList<Integer>();
       }
-      int longestNum = 0; //find this, get it's size, that's how many iterations we use
-      MyLinkedList<Integer> myData = new MyLinkedList<Integer>(); //put data into a linkedlist as variable to be updated each iteration
+      //go through all the data and
+      //a) find the longest num to determine num of iterations and
+      //b) move array data to linked list variable
+      int longestNum = 0;
+      MyLinkedList<Integer> myData = new MyLinkedList<Integer>();
+      //while finding the longest number place the data into the freqDig
       for (Integer i : data){
-        myData.add(i); //fill it while finding the longest num
-        if (i > longestNum){
+        Integer processing = data[i];
+        //placer tells you which bucket to move to
+        int placer = getDigit(processing, 1);
+        //if the number is positive, use this rule
+        if (processing >= 0){
+          digFreq[placer+10].add(processing);
+        }else{ //otherwise make sure to add it before all the positives
+          digFreq[9+placer].add(processing);
+        }
+        if (Math.abs(i) > longestNum){ //longest numbers hould include negatives
           longestNum = i;
         }
-      } //this is how many times thru the loop that we will do it's ok if numIters is 6 and most nums are two digits -- they  will stay sorted in the 0s list
-      System.out.println("longestNum: " + longestNum);
+      }
+      //now build my data from digFreq
+      if (myData.size() == 0){ //this should always be true, just a check
+        for (int d = 0; d<20; d++){
+          myData.extend(digFreq[d]); //add all the lists back to myData for next processing
+        }
+      }
       System.out.println("myData: " + myData.toString());
+
+      //now determine how many digits are in this longest num
       int numIters = Integer.toString(longestNum).length();
-      System.out.println("numIters: " + numIters);
-      for (int place = 1; place<=numIters; place++){
-        //fill out the digFreq board
-        int numItersC = myData.size(); //so that size doesn't update while loop is going
+      //go through data numIters time
+      for (int place = 2; place<=numIters; place++){
+        //so that size won't update in the loop below
+        int numItersC = myData.size();
+        //go through all of myData
         for (int c = 0; c<numItersC; c++){
           Integer processing = myData.removeFirst();
-          System.out.println("processing: " + processing);
-          int placer = getDigit(processing, place); //change getDigit to return 0 if u can't get that dig
-          System.out.println("placer: " + placer);
+          //placer tells you which bucket to move to
+          int placer = getDigit(processing, place);
+          //if the number is positive, use this rule
           if (processing >= 0){
-            System.out.println("going to add to list " + (placer+10) + " which looks like: ");
-            System.out.println(digFreq[placer+10].toString());
-            digFreq[placer+10].add(processing);//if it is a positive number, add it to its section
-          }else{ //means it's negative
-            digFreq[9-placer].add(processing);
+            digFreq[placer+10].add(processing);
+          }else{ //otherwise make sure to add it before all the positives
+            digFreq[9+placer].add(processing);
           }
+          //note: there are 20 buckets. bc getDigit returns 0 if
+          //no nth place exists for that number, no many how many iterations it will stay
+          //in the 0th buckets (which for negative numbers is 9 and for positive numbers is 10)
         }
         //now that you've processed data, rebuild it for next iteration:
-        if (myData.size() == 0){ //this should be true, just a check
+        if (myData.size() == 0){ //this should always be true, just a check
           for (int d = 0; d<20; d++){
             myData.extend(digFreq[d]); //add all the lists back to myData for next processing
           }
         }
       }
+      //rebuild data array to modify
       for (int e = 0; e<data.length; e++){
         data[e] = myData.removeFirst();
       }
+      System.out.println("Data: " + Arrays.toString(data));
   }
-  //% in java will give negative number -- be aware of this
+  //% in java will give negative number -- be aware of this -- this is why it is digFreq[9+placer]
   public static Integer getDigit(Integer num, int place){
-    if (place > (Integer.toString(num).length())){
-      //throw new IllegalArgumentException("place is greater than num digits");
-      return 0; //say you need 3rd of 8, that's 0
-    }
     Integer toReturn = 0;
+    if (place > (Integer.toString(num).length())){
+      return toReturn; //say you need 3rd of 8, that's 0. 8 = 008
+    }
     for (int i = 0; i<place; i++){
       toReturn = num%10;
       num/=10;
